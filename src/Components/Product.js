4ce48c78a6aec4dbe065a10a485cc8922ba877e4
Daniel Villarreal <cdvillarreal@gmail.com>
2022-05-10 13:50:35 -0300

    Trabajo terminado

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -13,7 +13,8 @@ const Product = ({
       <p className="descriptionProduct">{descriptionProduct}</p>
       <p className="priceProduct">{priceProduct}</p>
       <p className="skuProduct">SKU: {skuProduct}</p>
-      <p className="skuProduct">Stock: {quantityProduct}</p>
+      <p className="quantityProduct">Stock: {quantityProduct}</p>
+      <button className="buyButton">Comprar</button>
     </div>
   );
 };
